202326279a21c13d41c9f4fbcf4c834649e0dbc4
mar480 <1Z4v7m09n5x2!>
2019-06-25 08:59:13 -0400

    Completed MAC changer - pycharm GIT

--- mac_changer.py
+++ mac_changer.py
@@ -1,9 +1,28 @@
 #!/usr/bin/env python
 
-import subprocess
+import subprocess  # allows us to execute OS shell commands
+import optparse  # allows us to create and specify arguments and help text
 
-#Very insecure as users can add additional
-subprocess.call("ifconfig eth0 down", shell=True)
-subprocess.call("ifconfig eth0 hw ether 00:11:22:33:44:55", shell=True)
-subprocess.call("ifconfig eth0 up", shell=True)
 
+def get_arguments():
+    parser = optparse.OptionParser()  # create instance of object parser
+    parser.add_option("-i", "--interface", dest="interface", help="Interface whose MAC address you want to change")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+    (options, arguments) = parser.parse_args()  # so parser can read and understand the supplied arguments
+    if not options.interface:  # if no interface is specified
+        parser.error("[-] Please specify an interface. Use --help for more info.")
+    elif not options.new_mac:  # if no MAC address is specified
+        parser.error("[-] Please specify a new MAC address. Use --help for more info.")
+    return options  # return options so they can be accessed from global scope
+
+
+def change_mac(interface, new_mac):
+    print("[+] Changing MAC address for " + interface + " to " + new_mac + "\n")  # displays values of variables
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+    subprocess.call(["ifconfig", interface])  # so I don't have to keep typing it every time to check it worked!
+
+
+user_input = get_arguments()  # get user supplied interface and new MAC address from get_arguments function call
+change_mac(user_input.interface, user_input.new_mac)
